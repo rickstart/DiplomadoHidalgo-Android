465cd4cce4d0c42bc8400ab1107f03c4caa82f94
Ricardo Centeno Lugo <Rick@MBPdeRicardo2.lan>
2015-10-17 13:01:43 -0500

    MusicPlayer itemclickListener

--- MusicPlayer/app/src/main/java/com/mobintum/musicplayer/SongRVAdapter.java
+++ MusicPlayer/app/src/main/java/com/mobintum/musicplayer/SongRVAdapter.java
@@ -12,28 +12,37 @@ import java.util.List;
 /**
  * Created by Rick on 17/10/15.
  */
-public class SongRVAdapter  extends RecyclerView.Adapter<SongRVAdapter.ViewHolder>{
+public class SongRVAdapter extends RecyclerView.Adapter<SongRVAdapter.ViewHolder>{
     private List<Song> songs;
+    private SetOnItemClickListener mListener;
 
-    public SongRVAdapter(List<Song> songs) {
+    public SongRVAdapter(List<Song> songs, SetOnItemClickListener mListener) {
         this.songs = songs;
+        this.mListener = mListener;
     }
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.cardview_song,parent,false);
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.cardview_song, parent, false);
         ViewHolder holder = new ViewHolder(view);
         return holder;
     }
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-        Song song = songs.get(position);
+        final Song song = songs.get(position);
         holder.txtTitle.setText(song.getTitle());
         holder.txtAlbum.setText(song.getAlbum());
         holder.txtArtist.setText(song.getArtist());
         holder.txtTime.setText(song.getTime());
         holder.imgAlbum.setImageDrawable(song.getAlbumImage());
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mListener.itemClick(song);
+            }
+        });
 
 
     }
@@ -44,12 +53,13 @@ public class SongRVAdapter  extends RecyclerView.Adapter<SongRVAdapter.ViewHolde
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
+
         public final TextView txtTitle;
         public final TextView txtAlbum;
         public final TextView txtArtist;
         public final TextView txtTime;
         public final ImageView imgAlbum;
-
+        public final View itemView;
 
         public ViewHolder(View itemView) {
             super(itemView);
@@ -58,6 +68,17 @@ public class SongRVAdapter  extends RecyclerView.Adapter<SongRVAdapter.ViewHolde
             this.txtArtist = (TextView) itemView.findViewById(R.id.txtArtist);
             this.txtTime = (TextView) itemView.findViewById(R.id.txtTime);
             this.imgAlbum = (ImageView) itemView.findViewById(R.id.imgAlbum);
+            this.itemView = itemView;
+        }
+
+        public void setItemClickListener(View.OnClickListener listener){
+            itemView.setOnClickListener(listener);
         }
     }
+
+
+    public interface SetOnItemClickListener{
+        public void itemClick(Song song);
+    }
+
 }
